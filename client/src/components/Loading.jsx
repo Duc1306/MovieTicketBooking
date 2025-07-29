bd1b54220e5202d31d619114f66fb77cd6ee23f8
Duc1306 <trinhduc1362k3@gmail.com>
2025-07-29 11:24:10 +0700

    Movie Details Page: Structure & Features

---
+++ client/src/components/Loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div className='flex justify-center items-center h-[80vh]'>
+  <div className='animate-spin rounded-full h-14 w-14 border-2 border-t-primary border-primary border-t-4 border-solid border-t-transparent'></div>
+</div>
+  )
+}
+
+export default Loading
