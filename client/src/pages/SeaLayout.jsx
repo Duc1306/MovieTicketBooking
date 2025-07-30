b6dd075d27837bdf0ec9883dd6cb0b5d57d307c4
Duc1306 <trinhduc1362k3@gmail.com>
2025-07-30 16:04:25 +0700

    My Bookings Page & User History

--- client/src/pages/SeaLayout.jsx
+++ client/src/pages/SeaLayout.jsx
@@ -47,7 +47,7 @@ const SeaLayout = () => {
   };
 
   const renderSeats = (row, count = 9) => (
-    <div key={row} className="flex gap-2 mt-2">
+    <div key={row} classNamee="flex gap-2 mt-2">
       <div className="flex flex-wrap items-center justify-center gap-2">
         {Array.from({ length: count }, (_, i) => {
           const seatId = `${row}${i + 1}`;
@@ -70,50 +70,50 @@ const SeaLayout = () => {
     getShow();
   }, []);
   return show ? (
-    <div class="flex flex-col md:flex-row px-6 md:px-16 lg:px-40 py-30 md:pt-50">
+    <div className="flex flex-col md:flex-row px-6 md:px-16 lg:px-40 py-30 md:pt-50">
       {/* Available Timings */}
-      <div class="w-60 bg-primary/10 border border-primary/20 rounded-lg py-10 h-max md:sticky md:top-30">
-        <p class="text-lg font-semibold px-6">Available Timings</p>
-        <div class="mt-5 space-y-1">
+      <div className="w-60 bg-primary/10 border border-primary/20 rounded-lg py-10 h-max md:sticky md:top-30">
+        <p className="text-lg font-semibold px-6">Available Timings</p>
+        <div className="mt-5 space-y-1">
           {show.dateTime[date].map((item) => (
             <div
               key={item.time}
               onClick={() => setSelectedTime(item)}
-              class={`flex items-center gap-2 px-6 py-2 w-max rounded-r-md cursor-pointer transition ${
+              className={`flex items-center gap-2 px-6 py-2 w-max rounded-r-md cursor-pointer transition ${
                 selectedTime?.time === item.time
                   ? "bg-primary text-white"
                   : " hover:bg-primary/20"
               }`}
             >
-              <ClockIcon class=" w-4 h-4" />
+              <ClockIcon className=" w-4 h-4" />
 
-              <p class="text-sm">{isoTimeFormat(item.time)}</p>
+              <p className="text-sm">{isoTimeFormat(item.time)}</p>
             </div>
           ))}
         </div>
       </div>
 
       {/* Seats Layout */}
-      <div class="relative flex-1 flex flex-col items-center max-md:mt-16">
+      <div className="relative flex-1 flex flex-col items-center max-md:mt-16">
         <BlurCircle top="-100px" left="-100px" />
         <BlurCircle bottom="0px" right="-px" />
 
-        <h1 class="text-2xl font-semibold mb-4">Select your seat</h1>
+        <h1 className="text-2xl font-semibold mb-4">Select your seat</h1>
         <img alt="screen" src={assets.screenImage} />
-        <p class="text-gray-400 text-sm mb-6">SCREEN SIDE</p>
-        <div class="flex flex-col items-center mt-10 text-xs text-gray-300">
-          <div class="grid grid-cols-2 md:grid-cols-1 gap-8 md:gap-2 mb-6">
+        <p className="text-gray-400 text-sm mb-6">SCREEN SIDE</p>
+        <div className="flex flex-col items-center mt-10 text-xs text-gray-300">
+          <div className="grid grid-cols-2 md:grid-cols-1 gap-8 md:gap-2 mb-6">
             {groupRows[0].map((row) => renderSeats(row))}
           </div>
-          <div class="grid grid-cols-2 gap-11">
+          <div className="grid grid-cols-2 gap-11">
             {groupRows.slice(1).map((groupRows, idx) => (
               <div key={idx}>{groupRows.map((row) => renderSeats(row))}</div>
             ))}
           </div>
         </div>
-        <button onClick={()=> navigate('/my-bookings')} class="flex items-center gap-1 mt-20 px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer active:scale-95">
+        <button onClick={()=> navigate('/my-bookings')} className="flex items-center gap-1 mt-20 px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer active:scale-95">
           Proceed to Checkout
-          <ArrowRightIcon strokeWidth={3} class="w-4 h-4" />
+          <ArrowRightIcon strokeWidth={3} className="w-4 h-4" />
         </button>
       </div>
     </div>
