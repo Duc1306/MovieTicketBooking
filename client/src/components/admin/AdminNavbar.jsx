f4cbfc374a6bd445ef47bff22aef7d803796911f
Duc1306 <trinhduc1362k3@gmail.com>
2025-07-31 17:01:19 +0700

    Admin Pages: Booking/View Logic

--- client/src/components/admin/AdminNavbar.jsx
+++ client/src/components/admin/AdminNavbar.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 const AdminNavbar = () => {
   return (
-    <div class="flex items-center justify-between px-6 md:px-10 h-16 border-b border-gray-300/30">
+    <div className="flex items-center justify-between px-6 md:px-10 h-16 border-b border-gray-300/30">
       <Link to="/" >
         <img alt="logo" className="w-36 h-auto" src={assets.logo} />
       </Link>
--- client/src/pages/admin/AddShow.jsx
+++ client/src/pages/admin/AddShow.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Title from '../../components/admin/Title'
+import { dummyShowsData } from '../../assets/assets';
+import Loading from '../../components/Loading';
 
 const AddShow = () => {
-  return (
-    <div>AddShow</div>
-  )
+    const currency = import.meta.env.VITE_CURRENCY;
+
+  const [nowPlayingMovies, setNowPlayingMovies] = useState([]);
+  const [selectedMovie, setSelectedMovie] = useState(null);
+  const [dateTimeSelection, setDateTimeSelection] = useState({});
+  const [dateTimeInput, setDateTimeInput] = useState("");
+  const [showPrice, setShowPrice] = useState("");
+
+  const fetchnowPlayingMovies = async () =>{
+    setNowPlayingMovies(dummyShowsData)
+  }
+  useEffect(()=>{
+    fetchnowPlayingMovies()
+  },[])
+  return nowPlayingMovies.length >0 ?  (
+    <>
+     <Title text1="Add" text2="Shows" />
+    </>
+  ) : <Loading/>
 }
 
 export default AddShow
