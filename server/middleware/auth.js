273f1c300817df2ea3bed6bdf16c40a046793df4
Duc1306 <trinhduc1362k3@gmail.com>
2025-08-04 11:03:48 +0700

    Movies, Shows, Bookings: Backend Endpoints

---
+++ server/middleware/auth.js
@@ -0,0 +1,18 @@
+import { clerkClient } from "@clerk/express";
+
+export const protectAdmin = async (req, res, next) => {
+  try {
+    const { userId } = req.auth();
+
+    const user = await clerkClient.users.getUser(userId);
+
+    if (user.privateMetadata.role !== "Admin") {
+      return res.json({ success: false, message: "not authorzed" });
+    }
+
+    next();
+  } catch (error) {
+    console.error(error);
+    return res.json({ success: false, message: "not authorzed" });
+  }
+};
--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import { connectDB } from "./configs/db.js";
 import { clerkMiddleware } from '@clerk/express'
 import { serve } from "inngest/express";
 import { inngest, functions } from "./innegst/index.js"
+import showRoute from "./routes/showRoute.js";
 
 const app =express();
 
@@ -15,10 +16,12 @@ app.use(express.json());
 app.use(cors());
 app.use(clerkMiddleware())
 // Set up the "/api/inngest" (recommended) routes with the serve handler
-app.use("/api/inngest", serve({ client: inngest, functions }));
+
 
 // api routes
 app.get('/',(req,res)=>res.send('Server is Live!'))
+app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use('/api/show',showRoute)
 await connectDB()
 
 app.listen(port, ()=> console.log(`Server listening at http://localhost:${port}`)
