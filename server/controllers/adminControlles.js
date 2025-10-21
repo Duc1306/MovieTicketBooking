a7d9e5c805a53c6d1f7db8f1637730508dbd134d
Duc1306 <trinhduc1362k3@gmail.com>
2025-10-21 16:30:02 +0700

    admin dashboard

--- server/controllers/adminControlles.js
+++ server/controllers/adminControlles.js
@@ -11,9 +11,7 @@ export const isAdmin = async (req, res) => {
 export const getDashboardData = async (req, res) => {
   try {
     const bookings = await Booking.find({ isPaid: true });
-    const activeShows = await Show.find({
-      showDateTime: { $gte: new Date() },
-    }).populate("movie");
+    const activeShows = await Show.find().populate("movie");
 
     const totalUser = await User.countDocuments();
 
